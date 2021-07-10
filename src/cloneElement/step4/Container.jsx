4e2953d968076c9d9384e8cab555bb9a9e4e8871
wangy202012 <1143027179@qq.com>
2021-07-10 15:42:51 +0800

    auto save

--- src/cloneElement/step4/Container.jsx
+++ src/cloneElement/step4/Container.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useRequest } from 'ahooks';
 import qs from 'querystring';
 import { addAttrToValidSon } from './utils';
@@ -7,10 +7,25 @@ const mockUrl = 'https://mock.yonyoucloud.com/mock/16531/api/table/api/getList';
 const service = (params) => {
   return fetch(mockUrl + (params ? `?${qs.stringify(params)}` : '')).then((rst) => rst.json());
 };
-function Container({ children }) {
+
+const CState = ({ dataRef, runRef }) => {
   const { data, run } = useRequest(service);
-  const newChildren = addAttrToValidSon(children, () => ({ data, run }), (child) => child?.type?.name === 'Form');
-  return <div className="my-container">{newChildren}</div>;
+  dataRef.current = data;
+  runRef.current = run;
+  return <div />;
+};
+
+function Container({ children }) {
+  const dataRef = useRef();
+  const runRef = useRef();
+
+  const newChildren = addAttrToValidSon(children, () => ({ data: dataRef.current, run: runRef.current }), (child) => child?.type?.name === 'Form');
+
+  return (
+    <div className="my-container">
+      <CState dataRef={dataRef} runRef={runRef} />
+      {newChildren}
+    </div>);
 }
 
 export default Container;
